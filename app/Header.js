d1b4cb574962e50bf0a6d7de1786ea6dd45a718f
Katharina Simma <katharina.simma@diebin.at>
2023-02-05 21:01:28 +0100

    Add icons and logo

--- app/Header.js
+++ app/Header.js
@@ -1,3 +1,6 @@
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from 'next/image';
 import Link from 'next/link';
 import styles from './Header.module.scss';
 
@@ -5,14 +8,32 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <nav>
-        <div>
-          <Link href="/">Brick Base</Link>
+        <div className={styles.navLeft}>
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              width={40}
+              height={40}
+              className={styles.logo}
+            />
+          </Link>
+          <Link
+            href="/"
+            style={{
+              marginLeft: '5px',
+              marginRight: '10px',
+              fontSize: '1.5rem',
+            }}
+          >
+            Brick Base{' '}
+          </Link>
           <Link href="/shop" data-test-id="products-link">
             Shop
           </Link>
-
-          <Link href="/cart">cart</Link>
         </div>
+        <Link href="/cart">
+          <FontAwesomeIcon icon={faCartShopping} className={styles.icon} />
+        </Link>
       </nav>
     </header>
   );
