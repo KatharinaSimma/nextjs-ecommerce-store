53c6f2bf89375e896338035cb7efee42592266c7
Katharina Simma <katharina.simma@diebin.at>
2023-02-05 19:45:13 +0100

    Add cart total and empty cart message

--- app/cart/CartTable.js
+++ app/cart/CartTable.js
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { products } from '../../Database/products';
 import { getParsedCookie, setStringifiedCookie } from '../../utils/cookies';
@@ -8,6 +9,8 @@ import styles from './page.module.scss';
 
 export function CartTable() {
   const [cookieValue, setCookieValue] = useState(false);
+  let cartTotal = 0;
+  let cartNumberOfItems = 0;
 
   useEffect(() => {
     const valueOfTheCookie = getParsedCookie('cartItemCookie');
@@ -36,69 +39,90 @@ export function CartTable() {
 
   return (
     <main className={styles.main}>
-      {cartItemsCookieParsed.length > 0 ? (
-        cartItemsWithProductData.map((product) => {
-          if (product.amount > 0) {
-            return (
-              <div
-                className={styles.productCard}
-                key={product.id}
-                data-test-id={`cart-product-${product.id}`}
-              >
-                <div
-                  data-test-id={`product-${product.id}`}
-                  href={`/shop/${product.part}`}
-                  className={styles.productItem}
-                >
-                  <Image
-                    src={`/productImages/${product.image}`}
-                    alt={product.name}
-                    width="50"
-                    height="50"
-                  />
-                  <span>{product.name}</span>
-                  <span>Part no. {product.part}</span>
-                  <span>0.22€</span>
-                  <span data-test-id={`cart-product-quantity-${product.id}`}>
-                    Amount {product.amount}
-                  </span>
-                  <button
-                    onClick={() => {
-                      if (!cookieValue) {
-                        return;
-                      }
-                      const newCookie = [...cookieValue];
-                      const foundItem = newCookie.find((itemInCart) => {
-                        return itemInCart.id === product.id;
-                      });
+      {cartItemsWithProductData.map((product) => {
+        if (product.amount > 0) {
+          // add to cart total
+          cartTotal = cartTotal + product.amount * product.price;
+          // count the product
+          cartNumberOfItems = cartNumberOfItems + 1;
+          return (
+            <div
+              className={styles.productCard}
+              key={product.id}
+              data-test-id={`cart-product-${product.id}`}
+            >
+              <Image
+                src={`/productImages/${product.image}`}
+                alt={product.name}
+                width="50"
+                height="50"
+              />
+              <span>{product.name}</span>
+              <span>Part no. {product.part}</span>
+              <span>{product.price.toFixed(2)}</span>
+              <span data-test-id={`cart-product-quantity-${product.id}`}>
+                {product.amount}
+              </span>
+              <button
+                onClick={() => {
+                  if (!cookieValue) {
+                    return;
+                  }
+                  const newCookie = [...cookieValue];
+                  const foundItem = newCookie.find((itemInCart) => {
+                    return itemInCart.id === product.id;
+                  });
 
-                      if (foundItem) {
-                        foundItem.amount--;
-                        if (foundItem.amount < 0) {
-                          foundItem.amount = 0;
-                        }
-                      }
-                      setCookieValue(newCookie);
-                      setStringifiedCookie('cartItemCookie', newCookie);
-                    }}
-                  >
-                    -1
-                  </button>
-                </div>
-              </div>
-            );
-          }
-          return null;
-        })
-      ) : (
-        <div>... is empty. Go shop some bricks!</div>
+                  if (foundItem) {
+                    foundItem.amount--;
+                    if (foundItem.amount < 0) {
+                      foundItem.amount = 0;
+                    }
+                  }
+                  setCookieValue(newCookie);
+                  setStringifiedCookie('cartItemCookie', newCookie);
+                }}
+              >
+                -1
+              </button>
+              <button
+                data-test-id={`cart-product-remove-${product.id}`}
+                onClick={() => {
+                  if (!cookieValue) {
+                    return;
+                  }
+                  const newCookie = [...cookieValue];
+                  const foundItem = newCookie.find((itemInCart) => {
+                    return itemInCart.id === product.id;
+                  });
+
+                  if (foundItem) {
+                    foundItem.amount = 0;
+                  }
+                  setCookieValue(newCookie);
+                  setStringifiedCookie('cartItemCookie', newCookie);
+                }}
+              >
+                Remove
+              </button>
+            </div>
+          );
+        }
+        return null;
+      })}
+      {cartNumberOfItems === 0 && (
+        <div>
+          ... is empty. Go shop some bricks{' '}
+          <Link href="/shop">in the shop!</Link>
+        </div>
       )}
-      {cartItemsCookieParsed.length > 0 ? (
-        <>
-          <div data-test-id="cart-total">Cart Total: 500€</div>
-          <button data-test-id="cart-checkout">Go to checkout</button>
-        </>
-      ) : null}
+      <div>
+        Cart Total:{' '}
+        <span data-test-id="cart-total">{cartTotal.toFixed(2)}</span> €
+      </div>
+      <button disabled={cartNumberOfItems === 0} data-test-id="cart-checkout">
+        Go to checkout
+      </button>
     </main>
   );
 }
