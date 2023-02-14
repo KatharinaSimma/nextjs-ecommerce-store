0d57e5564681ee8c20bdcdeef5755a5f81cc8c9b
Katharina Simma <katharina.simma@diebin.at>
2023-02-14 18:11:45 +0100

    Add cash total to checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,3 +1,6 @@
+import { cookies } from 'next/headers';
+import { getProducts } from '../../database/products';
+import { CookieValue } from '../../utils/cookies';
 import CartCount from '../CartCount';
 import CheckoutForm from './checkoutForm';
 
@@ -7,7 +10,37 @@ export const metadata = {
     'This is the checkout page, add your personal data and review  products in the cart',
 };
 
-export default function CheckoutPage() {
+export const dynamic = 'force-dynamic';
+
+export default async function CheckoutPage() {
+  const products = await getProducts();
+  const cartCookie = cookies().get('cart');
+
+  let cartCookieParsed: CookieValue = [];
+  if (cartCookie) {
+    cartCookieParsed = JSON.parse(cartCookie.value);
+  }
+
+  const cartItemsWithProductData = products.map((product) => {
+    const fullCartItem = { ...product, amount: 0 };
+
+    const cartItem = cartCookieParsed.find(
+      (cookieItem) => product.id === cookieItem.id,
+    );
+    if (cartItem) {
+      fullCartItem.amount = cartItem.amount;
+    }
+
+    return fullCartItem;
+  });
+
+  const sumOfAllItems = cartItemsWithProductData.reduce(
+    (accumulator, currentValue) => {
+      return accumulator + currentValue.price * currentValue.amount;
+    },
+    0,
+  );
+
   return (
     <div>
       <h1>Checkout</h1>
@@ -16,7 +49,7 @@ export default function CheckoutPage() {
       <p>
         Number of Items: <CartCount />
       </p>
-      <p>Total Price:</p>
+      <p>Total Price: {sumOfAllItems.toFixed(2)}€</p>
       <hr />
       <CheckoutForm />
     </div>
